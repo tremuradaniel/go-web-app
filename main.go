e8b39c9b9030b2671257c922bc5965d46cf7b47c
Daniel Tremură <tremuradaniel@gmail.com>
2021-09-17 11:37:59 +0300

    functions and handlers

--- main.go
+++ main.go
@@ -5,15 +5,28 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8082"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the main page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, "This is the about page and 2 + 2 = %d", sum)
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	// _ = means that if there is an error, pay no attention to it
-	_ = http.ListenAndServe(":8082", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
