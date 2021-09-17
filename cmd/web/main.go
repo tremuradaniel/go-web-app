e950b54d857912db30149ca7b1f61081e262904e
Daniel Tremură <tremuradaniel@gmail.com>
2021-09-17 16:27:30 +0300

    Enabling Go Modules and refactoring our code to use packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"go-web-app/pkg/handlers"
 	"net/http"
 )
 
 const portNumber = ":8082"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,14 +1,15 @@
-package main
+package handlers
 
 import (
+	"go-web-app/pkg/render"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the main page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
