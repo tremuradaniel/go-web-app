fb7828987394992cb5c2ba2b26586cefeb6b7fd9
Daniel Tremură <tremuradaniel@gmail.com>
2021-09-17 14:17:06 +0300

    serving html templates

--- main.go
+++ main.go
@@ -1,55 +1,37 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8082"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the main page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	fmt.Fprintf(w, "This is the about page and 2 + 2 = %d", sum)
-}
-
-func addValues(x, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	division, err := divideValues(2.0, 0)
-	if err != nil {
-		fmt.Fprintf(w, fmt.Sprintf("%s", err))
-		return
-	}
-	fmt.Fprintf(w, fmt.Sprintf("This is the division page and x / y = %f", division))
-
-}
-
-func divideValues(x, y float64) (float64, error) {
-
-	if y == 0 {
-		err := errors.New("cannot divide by 0!")
-		return 0, err
-	}
-
-	result := x / y
-	return result, nil
+	renderTemplate(w, "about.page.tmpl")
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	// _ = means that if there is an error, pay no attention to it
 	_ = http.ListenAndServe(portNumber, nil)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template: ", err)
+		return
+	}
 }
