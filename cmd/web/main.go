416885d3b000169fd97b9de2a6df79e3a0578a91
Daniel Tremură <tremuradaniel@gmail.com>
2021-10-19 12:33:24 +0300

    installing and setting up a sessions package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,26 @@ import (
 	"go-web-app/pkg/render"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8082"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction // change to true in production
+	app.Session = session
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-web-app
 go 1.16
 
 require (
+	github.com/alexedwards/scs/v2 v2.4.0 // indirect
 	github.com/go-chi/chi v1.5.4
 	github.com/justinas/nosurf v1.1.1 // indirect
 )
